3178ef8ab76c7a5f20e89d0a4e2caea5313f4b40
Anton Khizunov <anton.khizunov@clarizen.com>
2019-10-07 23:25:05 +0300

    implement binary search

---
+++ src/main/java/com/khizunov/binary/com/khizunov/App.java
@@ -0,0 +1,26 @@
+package com.khizunov;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.khizunov.binary.Algorithms;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 10, 34, 54, 100);
+        list.forEach(i -> System.out.println(i));
+        int index = Algorithms.binarySearch(10, list);
+        System.out.println("Found index: " + index);
+
+    }
+}
