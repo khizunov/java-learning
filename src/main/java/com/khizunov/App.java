6c0f5afeb60b2b06f2e5a8dc5a4cc3664a19cd3b
Anton Khizunov <anton.khizunov@clarizen.com>
2019-10-08 18:01:24 +0300

    implement not rexursive binary search

--- src/main/java/com/khizunov/App.java
+++ src/main/java/com/khizunov/App.java
@@ -5,18 +5,12 @@ import java.util.List;
 
 import com.khizunov.binary.Algorithms;
 
-/**
- * Hello world!
- */
+
 public class App {
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 10, 34, 54, 100);
         list.forEach(i -> System.out.println(i));
-        int index = Algorithms.binarySearch(10, list);
+        int index = Algorithms.binarySearch(100, list, false);
         System.out.println("Found index: " + index);
     }
 }
--- src/main/java/com/khizunov/binary/Algorithms.java
+++ src/main/java/com/khizunov/binary/Algorithms.java
@@ -1,21 +1,23 @@
 package com.khizunov.binary;
 
-
 import java.util.List;
 
 public class Algorithms {
 
-    public static int binarySearch(Integer key, List<Integer> sorted) {
-        return search(key, sorted, 0, sorted.size());
+    public static int binarySearch(Integer key, List<Integer> sorted, boolean isRecursive) {
+        if (isRecursive) {
+            return recursiveBinarySearch(key, sorted, 0, sorted.size());
+        }
+
+        return search(key, sorted);
     }
 
-    private static int search(Integer key, List<Integer> sorted, int begin, int end) {
+    private static int recursiveBinarySearch(Integer key, List<Integer> sorted, int begin, int end) {
         if (end < begin) {
             return -1;
         }
 
         int middle = (begin + end)/2;
-
         Integer current = sorted.get(middle);
 
         if (current == key) {
@@ -23,9 +25,31 @@ public class Algorithms {
         }
 
         if (key > current) {
-            return search(key, sorted, middle + 1, end);
+            return recursiveBinarySearch(key, sorted, middle + 1, end);
         }
 
-        return search(key, sorted, begin, middle - 1);
+        return recursiveBinarySearch(key, sorted, begin, middle - 1);
+    }
+
+    private static int search(Integer key, List<Integer> sorted) {
+        int begin = 0;
+        int end = sorted.size();
+
+        while (begin < end) {
+            int middle = (begin + end)/2;
+            Integer current = sorted.get(middle);
+
+            if (current == key) {
+                return middle;
+            }
+
+            if (key > current) {
+                 begin = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+
+        return -1;
     }
 }
